3f073beec6261bdbd1e0edfae2e465805a07084a
Youcef <youcefcopy@gmail.com>
2026-01-26 16:01:42 +0100

    creating the production_year const

--- schema/002_constraints.sql
+++ schema/002_constraints.sql
@@ -1,3 +1,6 @@
 -- creating the constraints here
 
--- ! 1- foregin keys constraints
+-- ! 1- production_year constraint
+ALTER TABLE Car
+ADD CONSTRAINT production_year_const
+CHECK (EXTRACT(YEAR FROM production_year) <= EXTRACT(YEAR FROM NOW()));

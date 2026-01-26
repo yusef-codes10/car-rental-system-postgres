53b294257a664eccbf52b22033804c7510db6c99
Youcef <youcefcopy@gmail.com>
2026-01-26 13:11:09 +0100

    creating the Car table

--- schema/001_create_tables.sql
+++ schema/001_create_tables.sql
@@ -1,5 +1,10 @@
 CREATE TABLE Car (
-
+    id BIGSERIAL PRIMARY KEY,
+    model VARCHAR(30),
+    make VARCHAR(30),
+    production_year DATE,
+    availability VARCHAR(10),
+    hourly_rental_price NUMERIC(10,2)
 );
 
 CREATE TABLE Customer (

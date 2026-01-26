0ea6cb70560d1cfee0d5c4265c2365a6a06b439b
Youcef <youcefcopy@gmail.com>
2026-01-26 16:02:38 +0100

    NOW() for timestamp & CURRENT_DATE for Date

--- schema/002_constraints.sql
+++ schema/002_constraints.sql
@@ -3,4 +3,4 @@
 -- ! 1- production_year constraint
 ALTER TABLE Car
 ADD CONSTRAINT production_year_const
-CHECK (EXTRACT(YEAR FROM production_year) <= EXTRACT(YEAR FROM NOW()));
+CHECK (EXTRACT(YEAR FROM production_year) <= EXTRACT(YEAR FROM CURRENT_DATE));

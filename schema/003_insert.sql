fa0e6af1cd6f87d81392a9fc162b05f9e8fa92fd
Youcef <youcefcopy@gmail.com>
2026-01-27 10:06:19 +0100

    inserting cars

---
+++ schema/003_insert.sql
@@ -0,0 +1,23 @@
+-- here we are going to insert some values
+
+INSERT INTO Car (model, make, production_year, availability, hourly_rental_price) VALUES
+('Model S', 'Tesla', '2020-01-01', 'Available', 25.00),
+('Civic', 'Honda', '2018-05-01', 'Rented', 15.00),
+('Corolla', 'Toyota', '2017-03-01', 'Available', 14.00),
+('Mustang', 'Ford', '2019-07-01', 'Breakdown', 30.00),
+('Camry', 'Toyota', '2021-06-01', 'Available', 18.00),
+('Accord', 'Honda', '2020-04-01', 'Rented', 17.00),
+('A4', 'Audi', '2016-09-01', 'Available', 22.00),
+('A6', 'Audi', '2018-11-01', 'Available', 24.00),
+('X5', 'BMW', '2019-02-01', 'Rented', 28.00),
+('X3', 'BMW', '2017-08-01', 'Available', 23.00),
+('Charger', 'Dodge', '2020-10-01', 'Available', 26.00),
+('Explorer', 'Ford', '2016-12-01', 'Breakdown', 20.00),
+('Elantra', 'Hyundai', '2019-01-01', 'Available', 13.00),
+('Sonata', 'Hyundai', '2021-03-01', 'Rented', 16.00),
+('Impala', 'Chevrolet', '2015-05-01', 'Available', 12.00),
+('Malibu', 'Chevrolet', '2018-06-01', 'Available', 14.00),
+('Altima', 'Nissan', '2019-09-01', 'Available', 15.00),
+('Sentra', 'Nissan', '2016-04-01', 'Available', 11.00),
+('Wrangler', 'Jeep', '2020-07-01', 'Rented', 27.00),
+('Cherokee', 'Jeep', '2017-02-01', 'Available', 19.00);

d10ec00a7a49b61835b02389fece4965c3d44f3c
Youcef <youcefcopy@gmail.com>
2026-01-27 11:10:47 +0100

    subqueries are redundant, cleaner versions with inner join | left join istead of not in

--- queries/joins.sql
+++ queries/joins.sql
@@ -1,9 +1,9 @@
--- Show me all rentals with the car model and the customer’s full name.
+--!1- Show me all rentals with the car model and the customer’s full name.
 SELECT first_name, last_name, model FROM rental 
 JOIN Customer on customer.id = rental.costumer_id
 JOIN car on car.id = rental.car_id;
 
--- List all cars that have ever been rented at least once.
+--!2- List all cars that have ever been rented at least once.
 SELECT DISTINCT model, make FROM Car 
 JOIN Rental on rental.car_id = car.id
 where car.id in (
@@ -11,7 +11,12 @@ where car.id in (
 
 );
 
--- List all customers who have rented a car at least once.
+        -- ? cleaner version
+        SELECT DISTINCT c.model, c.make
+        FROM car c
+        JOIN rental r ON r.car_id = c.id;
+
+--!3- List all customers who have rented a car at least once.
 SELECT DISTINCT first_name, last_name FROM Customer 
 JOIN Rental on rental.costumer_id = Customer.id
 where Customer.id in (
@@ -19,12 +24,18 @@ where Customer.id in (
 
 );
 
--- Show all rentals that have not been returned yet, including car model and customer name.
+        -- ? cleaner version
+        SELECT DISTINCT cu.first_name, cu.last_name
+        FROM customer cu
+        JOIN rental r ON r.costumer_id = cu.id;
+
+
+--!4- Show all rentals that have not been returned yet, including car model and customer name.
 SELECT first_name, last_name, model, make FROM Customer
 JOIN rental on rental.costumer_id = Customer.id
 JOIN car on rental.car_id = car.id
 where rental.return_date is null;
 
--- Show each rental with car make, model, and rental start & end date.
+--!5- Show each rental with car make, model, and rental start & end date.
 SELECT model, make, start_date, end_date FROM rental
 JOIN car on car.id = rental.car_id;

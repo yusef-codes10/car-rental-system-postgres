e49ac56ee71bec6f028af185d66b27d3d4a18a0b
Youcef <youcefcopy@gmail.com>
2026-01-27 10:35:43 +0100

    2nd query

--- queries/joins.sql
+++ queries/joins.sql
@@ -2,3 +2,11 @@
 SELECT first_name, last_name, model FROM rental 
 JOIN Customer on customer.id = rental.costumer_id
 JOIN car on car.id = rental.car_id;
+
+-- List all cars that have ever been rented at least once.
+SELECT DISTINCT model, make FROM Car 
+JOIN Rental on rental.car_id = car.id
+where car.id in (
+    SELECT car_id from rental
+
+);

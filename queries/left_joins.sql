00ac5d754e7a3847b491e5f64ad8496180cddab7
Youcef <youcefcopy@gmail.com>
2026-01-28 14:48:21 +0100

    forgot to add LEFT

--- queries/left_joins.sql
+++ queries/left_joins.sql
@@ -2,6 +2,10 @@
 
 -- !1- Show all cars, even those that were never rented.
 SELECT car.* FROM car 
-JOIN rental on rental.car_id = car.id;
+LEFT JOIN rental on rental.car_id = car.id;
 
     -- ? what if we just showed the entire cars
+
+-- !2- Show all customers, even those who never rented a car.
+SELECT Customer.* From Customer
+LEFT JOIN rental on rental.costumer_id = customer.id;

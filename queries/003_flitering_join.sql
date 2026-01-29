77582b0fe3bd6bf1e17f0eef2ff2359a9d42de3f
Youcef <youcefcopy@gmail.com>
2026-01-29 16:19:00 +0100

    lvl3 3rd query

--- queries/003_flitering_join.sql
+++ queries/003_flitering_join.sql
@@ -6,3 +6,9 @@ WHERE car.availability = 'Rented';
 --!2- Show rentals where the return date is NULL but the end date has already passed.
 SELECT rental.* FROM rental
 WHERE return_date IS NULL AND end_date > NOW()::DATE;
+
+--!3- List customers who currently have a car rented (not yet returned).
+SELECT customer.* FROM Customer
+JOIN rental on customer.id = rental.costumer_id
+JOIN car on car.id = rental.car_id
+WHERE car.availability = 'Rented' AND rental.return_date IS NULL;

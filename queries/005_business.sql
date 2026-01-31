2deb240c4d5198e8bc936eb8d98651b2a76bf5e4
Youcef <youcefcopy@gmail.com>
2026-01-31 18:52:47 +0100

    lvl5 1st |  customers who rented the same car more than once

--- queries/005_business.sql
+++ queries/005_business.sql
@@ -1,9 +1,16 @@
 --!1- Find customers who rented the same car more than once
 -- find a car that has been rented more than once
-SELECT car.model, car.make, count(*)
+SELECT car.model,
+       car.make,
+       count(*),
+       customer.first_name,
+       customer.last_name
 FROM car
 JOIN rental on rental.car_id = car.id
+JOIN Customer on customer.id = rental.costumer_id
 GROUP BY rental.car_id,
          car.model,
-         car.make
+         car.make,
+         customer.first_name,
+         customer.last_name
 HAVING count(*) >= 2;

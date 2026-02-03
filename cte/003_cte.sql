543517eb7fd3941633dc4ae71b1199a3f8c1ba85
Youcef <youcefcopy@gmail.com>
2026-02-03 16:01:30 +0100

    cross join version

--- cte/003_cte.sql
+++ cte/003_cte.sql
@@ -7,3 +7,13 @@ SELECT car.*
 FROM car
 CROSS JOIN avg_rental
 WHERE car.hourly_rental_price >= avg_rental.avg_price;
+
+--!2- Retrieve the first name, last name, and date of birth of the customer who is the oldest in the database.
+WITH oldest_date AS (
+    SELECT MIN(date_of_birth) as min_value
+    FROM customer
+)
+SELECT customer.first_name, customer.last_name, customer.date_of_birth
+FROM customer
+CROSS JOIN oldest_date
+WHERE customer.date_of_birth = oldest_date.min_value;
